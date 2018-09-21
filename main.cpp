d5e5c9c23951b699d6ba32084ebee31a107410b7
k-chaikina <43457832+k-chaikina@users.noreply.github.com>
2018-09-21 04:26:31 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+void f1()
+{
+    std::cout << " 1 2 3 4 5 " << std::endl;
+}
+
+//using namespace std;
+
+int main()
+{
+    f1();
+    std::cout << " 6 7 8 9 0 " << std::endl;
+    
+    return 0;
+}
+
+
